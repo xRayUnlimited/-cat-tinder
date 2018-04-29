ebcf1e6e40d2effc7c85710f8490615855ccac84
Ray Suyoto <ray.suy@icloud.com>
2018-04-28 23:54:43 -0600

    correction

--- client/src/components/DeleteAccount.js
+++ client/src/components/DeleteAccount.js
@@ -1,3 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 
+class DeleteAccount extends Component {
+  render() {
+    return (
+      <div></div>
+    )}
+}
+export default DeleteAccount;
